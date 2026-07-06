6cb04584a6f7c3b4baafb9531712b48d0e4d17e6
ramudaeevee <72556715+ramudaeevee@users.noreply.github.com>
2026-07-06 18:47:54 +0900

    諸々のシステム実装

--- Assets/script/ChunkManager.cs
+++ Assets/script/ChunkManager.cs
@@ -4,7 +4,9 @@ public class StageGenerator : MonoBehaviour
 {
     public GameObject roadPrefab;
     public GameObject cliffPrefab;
+    public GameObject pointPrefab;
     public Transform player;
+    public Vector3 lastRoadPos;
 
     public int maxDistance = 2000;
     public int minDistance = 2000;
@@ -20,12 +22,17 @@ public class StageGenerator : MonoBehaviour
         int Dist = Random.Range(minDistance, maxDistance+1);
         int count = Mathf.CeilToInt(Dist / chunkSize);
 
-        Vector3 startPos = player.position + new Vector3(chunkSize * 0.5f, 0f, 0f);;
+        Vector3 startPos = player.position + new Vector3(chunkSize * 0.5f, 0f, 0f);
 
         for (int i = 0; i < count; i++)
         {
             Vector3 pos = startPos + new Vector3(i * chunkSize, 0f, 0f);
             Instantiate(roadPrefab, pos, Quaternion.identity);
+            if (i == count - 1)
+            {
+                lastRoadPos = pos + new Vector3(chunkSize * 0.5f, 1f, 0f);
+                Instantiate(pointPrefab, lastRoadPos, Quaternion.identity);
+            }
         }
 
         // 崖を最後に配置
